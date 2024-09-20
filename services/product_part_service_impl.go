fb7aa1913a0c84d8c9ac46250a779aab2120bb2b
gahyun <gahyunlee1119@gmail.com>
2024-09-20 16:36:31 -0400

    working on product part

---
+++ services/product_part_service_impl.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"main-admin-api/models"
+	"main-admin-api/repository"
+)
+
+type ProductPartServiceImpl struct {
+	productPartRepository repository.ProductPartRepositoryInterface
+}
+
+func NewProductPartServiceImpl(repository repository.ProductPartRepositoryInterface) ProductPartServiceInterface {
+	return &ProductPartServiceImpl{productPartRepository: repository}
+}
+
+func (s *ProductPartServiceImpl) CreateProductPart(productPart *models.ProductPart) error {
+	return s.productPartRepository.Create(productPart)
+}
+
+func (s *ProductPartServiceImpl) GetProductPartByID(id uint) (*models.ProductPart, error) {
+	return s.productPartRepository.GetByID(id)
+}
+
+func (s *ProductPartServiceImpl) GetAllProductPart() (*[]models.ProductPart, error) {
+	return s.productPartRepository.GetAll()
+}
+
+func (s *ProductPartServiceImpl) UpdateProductPart(productPart *models.ProductPart) error {
+	return s.productPartRepository.Update(productPart)
+}
+
+func (s *ProductPartServiceImpl) DeleteProductPart(id uint) error {
+	return s.productPartRepository.Delete(id)
+}
---
+++ services/product_part_service_interface.go
@@ -0,0 +1,11 @@
+package services
+
+import "main-admin-api/models"
+
+type ProductPartServiceInterface interface {
+	CreateProductPart(productPart *models.ProductPart) error
+	GetProductPartByID(id uint) (*models.ProductPart, error)
+	GetAllProductPart() (*[]models.ProductPart, error)
+	UpdateProductPart(productPart *models.ProductPart) error
+	DeleteProductPart(id uint) error
+}
