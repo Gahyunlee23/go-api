1947840c8b74cf64ffec72f605e0f49127ae6cf0
gahyun <gahyunlee1119@gmail.com>
2024-10-10 18:06:26 -0400

    working on file type

--- internal/repository/interfaces/file_type.go
+++ internal/repository/interfaces/file_type.go
@@ -1,4 +1,16 @@
 package repository
 
+import (
+	"main-admin-api/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
 type FileTypeRepository interface {
+	Create(fileType *models.FileType) error
+	GetByID(id uint) (*models.FileType, error)
+	GetAll(ctx *gin.Context) ([]models.FileType, error)
+	Update(fileType *models.FileType) error
+	Archive(id uint) error
+	Count(ctx *gin.Context) (int64, error)
 }
