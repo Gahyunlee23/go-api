8a09754792fca2753f7f02336721b0c2007bdedc
gahyun <gahyunlee1119@gmail.com>
2024-10-11 13:08:59 -0400

    working on file type

---
+++ internal/api/routes/file_type.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	handler "main-admin-api/internal/api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func FileTypeRoute(router *gin.Engine, handler *handler.FileTypeHandler) {
+	fileTypeGroup := router.Group("/file-type")
+	{
+		fileTypeGroup.GET("/", handler.GetAllFileTypes)
+		fileTypeGroup.POST("/", handler.CreateFileType)
+		fileTypeGroup.PUT("/:id", handler.UpdateFileType)
+		fileTypeGroup.GET(":id", handler.GetFileTypeByID)
+		fileTypeGroup.DELETE("/:id", handler.DeleteFileType)
+	}
+}
